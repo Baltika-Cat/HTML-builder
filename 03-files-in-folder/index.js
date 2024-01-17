32be67f10eea3e0e3249fd64643a64dcc60412d8
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-01-18 02:45:54 +0300

    feat: complete files-in-folder task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,22 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const pathFolder = path.join(`${__dirname}/secret-folder`);
+const allFiles = fs.readdir(pathFolder, {withFileTypes: true}, (err, files) => {
+  if (err) {
+    console.log (err);
+  }
+  files.forEach ((file) => {
+    if (file.isFile()) {
+      fs.stat(`${pathFolder}/${file.name}`, (err, stats) => {
+        if (err) {
+          console.log (err);
+        }
+        let name = path.parse(file.name).name;
+        let extension = `${path.extname(`${pathFolder}/${file.name}`)}`;
+        let size = `${(stats.size/1024).toFixed(3)}kb`;
+        console.log ('\x1b[36m', `${name} - ${extension} - ${size}`, '\x1b[37m');
+      });
+    }
+  })
+});
