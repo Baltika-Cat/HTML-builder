80ef6a12a41b36e5c8f83b49631b02002f4f17ba
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-01-18 03:54:40 +0300

    feat: complete copy-direction task

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,28 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+const oldFolder = path.join(__dirname, 'files');
+const newFolder = path.join(__dirname, 'files-copy');
+
+fs.mkdir(newFolder, {recursive: true}, (err) => {
+  if (err) {
+    console.log ('Directory is already created');
+  }
+})
+
+fs.readdir(oldFolder, {withFileTypes: true}, (err, files) => {
+  if (err) {
+    console.log (err);
+  }
+  files.forEach ((file) => {
+    if (file.isFile()) {
+      let oldFile = path.join(oldFolder, file.name);
+      let newFile = path.join(newFolder, file.name);
+      fs.copyFile(oldFile, newFile, (err) => {
+        if (err) {
+          console.log('File already exists in the destination directory');
+        }
+      })
+    }
+  })
+})
