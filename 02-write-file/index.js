afe52ca83725bf9d73c3ac3c8a07c8055b65220b
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2024-01-22 02:31:40 +0300

    fix: fix open folder after text input

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,25 +1,26 @@
 const fs = require('node:fs');
 const path = require('node:path');
+
+const fileName = path.join(__dirname, 'text.txt');
+fs.open(fileName, 'a', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+
 const readline = require('node:readline');
 const io = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-const fileName = 'text.txt';
-fs.open(fileName, 'a', (err) => {
-  if (err) {
-    throw err;
-  }
-});
-
 console.log('\x1b[40m', '\x1b[32m', 'Hello! Write your text, please', '\x1b[37m');
 
 io.on ('line', (input) => {
   if (input !== 'exit') {
-    fs.appendFile(pathFile, `${input}\n`, (err) => {
+    fs.appendFile(fileName, `${input}\n`, (err) => {
       if (err) {
-        throw err;
+        console.log(err);
       }
     });
   } else {
@@ -30,5 +31,3 @@ io.on ('line', (input) => {
 io.on ('close', () => {
   console.log('\x1b[32m', 'Goodbye!', '\x1b[37m');
 })
-
-const pathFile = path.join(`${__dirname}/${fileName}`);
